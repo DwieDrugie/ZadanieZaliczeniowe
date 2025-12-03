85a2fa1fc869ab4b0f051568050ef963fe71731f
DwieDrugie <maciskliskwi@gmail.com>
2025-12-03 14:08:05 +0100

    Commit

--- app/src/main/java/com/example/zadaniezalicz/MainActivity.java
+++ app/src/main/java/com/example/zadaniezalicz/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.zadaniezalicz;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +34,51 @@ public class MainActivity extends AppCompatActivity {
         );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        Button btnGen = findViewById(R.id.btnGenerate);
+        Button btnConf = findViewById(R.id.btnConfirm);
+        EditText numQua = findViewById(R.id.numbersQuantity);
+        EditText name = findViewById(R.id.name);
+        EditText surname = findViewById(R.id.surname);
+        CheckBox letters = findViewById(R.id.Letters);
+        CheckBox numbers = findViewById(R.id.numbers);
+        CheckBox spChar = findViewById(R.id.specialChar);
+
+        String chars = "abcdefghijklmnopqrstuvwxyz";
+
+
+
+        btnGen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String qua = numQua.getText().toString();
+                int lengths = Integer.parseInt(qua);
+                String chars = "abcdefghijklmnopqrstuvwxyz";
+                String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                String numbers = "0123456789";
+                String spChar = "!@#$%^&*()_-+=<>?/{}~|";
+                Random rn = new Random();
+                StringBuilder sb = new StringBuilder(lengths);
+                if(letters.isChecked()) {
+                    chars += upperCase;
+                    sb.append(upperCase.charAt(rn.nextInt(upperCase.length()-1)));
+
+                }
+
+
+
+                for(int i=sb.length();i < lengths;++i){
+                    sb.append(chars.charAt(rn.nextInt(chars.length())));
+                }
+
+
+
+
+
+
+            }
+        });
     }
+
+
 }
